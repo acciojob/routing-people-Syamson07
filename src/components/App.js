82efb1b01feb528410b7d21b07830cb13684e180
Syam <syam.penumala225@gmail.com>
2025-06-08 18:08:58 +0530

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import UserList from "./UserList";
+import UserDetails from "./UserDetails";
+
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={UserList} />
+        <Route path="/users/:id" component={UserDetails} />
+      </Switch>
+    </Router>
+  );
+}
