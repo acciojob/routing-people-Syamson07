3a2d3c7e567171ee67b049e688269db15729d19b
Syam Son Penumala <syamson132000@gmail.com>
2025-06-08 18:18:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import UserList from "./UserList";
 import UserDetails from "./UserDetails";
 
 export default function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<UserList />} />
-        <Route path="/users/:id" element={<UserDetails />} />
-      </Routes>
+      <Switch>
+        <Route exact path="/" component={UserList} />
+        <Route path="/users/:id" component={UserDetails} />
+      </Switch>
     </Router>
   );
 }
